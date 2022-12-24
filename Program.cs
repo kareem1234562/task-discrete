c807c3f7077ff8f8d8398ac67f9b9284777db5cb
kareem1234562 <121341685+kareem1234562@users.noreply.github.com>
2022-12-24 13:47:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discrete_yask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entre first number ");
+            int x = int.Parse(Console.ReadLine());        
+            Console.WriteLine("entre last number ");
+            int y = int.Parse(Console.ReadLine());
+
+
+            Boolean f = true;
+            for (int j = x; j <= y; j++)
+            {
+
+                for (int o = 2; o < j; o++)
+                {
+
+
+                    if (j % o == 0)
+                    {
+
+                        f = false;
+                        break;
+
+                    }
+                    else 
+                    {
+                        f = true;
+                        continue;
+                    }
+                
+                
+                
+                
+                
+                
+                
+                
+                }
+            
+            
+            
+            if(f==true)
+                    Console.WriteLine(j);
+            
+            
+            
+            
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
